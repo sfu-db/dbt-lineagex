975bae28ef729a4cbe26cf857d6bf35d160f8f13
zshandy <zshandy@gmail.com>
2023-04-20 18:06:56 -0700

    init

--- scripts/main.py
+++ scripts/main.py
@@ -2,4 +2,4 @@ from lineage import Lineage
 import os
 
 if __name__ == '__main__':
-    lineage_output = Lineage(os.path.dirname(os.getcwd()))
+    lineage_output = Lineage(os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd()))))
