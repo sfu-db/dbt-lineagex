ae7b89a4edf5a5142a9155d13abb09b116ac5479
zshandy <zshandy@gmail.com>
2023-05-13 14:53:24 -0700

    bash file

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-pip install -r $SCRIPT_DIR/scripts/requirements.txt
-#python3 $SCRIPT_DIR/scripts/main.py
+pip install -r requirements.txt
+python3 main.py
