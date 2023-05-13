a426fb76604a125d3b46b3be5c4e18404ac267b5
zshandy <zshandy@gmail.com>
2023-05-12 18:23:23 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-pip install -r scripts/requirements.txt
-python3 scripts/main.py
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pip install -r SCRIPT_DIR/scripts/requirements.txt
+python3 SCRIPT_DIR/scripts/main.py
